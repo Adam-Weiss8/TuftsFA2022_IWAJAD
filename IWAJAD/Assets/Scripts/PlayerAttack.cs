e05f60adde83eedc29f5b8b52e2d0755bca9d00a
Jfreedman501 <98427898+Jfreedman501@users.noreply.github.com>
2022-12-03 10:50:21 -0500

    Single Enemy (no damage to player)

---
+++ IWAJAD/Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour{
+
+      //public Animator anim;
+      public Transform attackPt;
+      public float attackRange = 0.5f;
+      public float attackRate = 2f;
+      private float nextAttackTime = 0f;
+      public int attackDamage = 40;
+      public LayerMask enemyLayers;
+
+      void Start(){
+           //anim = gameObject.GetComponentInChildren<Animator>();
+      }
+
+      void Update(){
+           if (Time.time >= nextAttackTime){
+                  //if (Input.GetKeyDown(KeyCode.Space))
+                 if (Input.GetAxis("attack") > 0){
+                        Attack();
+                        nextAttackTime = Time.time + 1f / attackRate;
+                  }
+            }
+      }
+
+      void Attack(){
+            //anim.SetTrigger ("Melee");
+            Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPt.position, attackRange, enemyLayers);
+           
+            foreach(Collider2D enemy in hitEnemies){
+                  Debug.Log("We hit " + enemy.name);
+                  enemy.GetComponent<Enemy>().TakeDamage(attackDamage);
+            }
+      }
+
+      //NOTE: to help see the attack sphere in editor:
+      void OnDrawGizmosSelected(){
+           if (attackPt == null) {return;}
+            Gizmos.DrawWireSphere(attackPt.position, attackRange);
+      }
+}
