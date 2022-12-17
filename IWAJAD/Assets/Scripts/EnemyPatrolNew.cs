17c771ee6a7ca0daebd626a0bb9789f44b96332e
Jfreedman501 <98427898+Jfreedman501@users.noreply.github.com>
2022-12-17 14:29:17 -0500

    BIG BOSS BIG HURT

--- IWAJAD/Assets/Scripts/EnemyPatrolNew.cs
+++ IWAJAD/Assets/Scripts/EnemyPatrolNew.cs
@@ -17,18 +17,10 @@ public class EnemyPatrolNew : MonoBehaviour {
 	   public float startDazedTime;
 	   public bool hasNotFoundEdge = true;
 
-
-       //public int damage = 10;
-       //private GameHandler gameHandler;
 		private float rayLength = 0.8f;
 
        void Start(){
-			//anim = GetComponent<Animator>();
 			rb = GetComponent<Rigidbody2D>();
-			//anim.SetBool("Walk", true);
-			//if (GameObject.FindWithTag ("GameHandler") != null) {
-			//    gameHandler = GameObject.FindWithTag ("GameHandler").GetComponent<GameHandler> ();
-			//}
        }
 
 	void Update(){
@@ -59,38 +51,21 @@ public class EnemyPatrolNew : MonoBehaviour {
 			hasNotFoundEdge = true;
 		} else {
 			isFacingRight = !isFacingRight;
-			transform.localScale = new Vector3(-transform.localScale.x, 1f, 1f);
+			transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, 1f);
 		}
 		
 		//wall flipping
 		if (hitFwd.collider != false){
 			Debug.Log("I hit a wall: ");
 			isFacingRight = !isFacingRight;
-			transform.localScale = new Vector3(-transform.localScale.x, 1f, 1f);   
+			transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, 1f);   
 		}
 		
 	}
-
-	//public void OnCollisionEnter2D(Collision2D other){
-              //if (other.gameObject.tag == "Player") {
-                     //anim.SetBool("Attack", true);
-                     //gameHandlerObj.playerGetHit(damage);
-                     //rend.material.color = new Color(2.4f, 0.9f, 0.9f, 0.5f);
-                     //StartCoroutine(HitEnemy());
-              //}
-       //}
-
-       //public void OnCollisionExit2D(Collision2D other){
-       //       if (other.gameObject.tag == "Player") {
-                     //anim.SetBool("Attack", false);
-       //       }
-       //}
 	   
 	void OnDrawGizmosSelected(){
               Gizmos.DrawWireSphere(groundCheck.position, rayLength);
 			  Gizmos.DrawWireSphere(groundCheck.position, rayLength/2);
-			  //Debug.DrawLine(groundCheck.position, -transform.up, Color.red, rayLength);
-			  //Debug.DrawLine(groundCheck.position, -transform.right, Color.red, rayLength/2);
     }
 	   
 }
